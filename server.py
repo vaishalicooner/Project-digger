f6a877794e552c18b69b418599b64c513a90053d
Vaishali Cooner <vaishalicooner@gmail.com>
2018-11-06 17:27:21 +0000

    working on front end

--- server.py
+++ server.py
@@ -341,7 +341,7 @@ if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
 
-    app.debug = True
+    # app.debug = True
    
     app.jinja_env.auto_reload = app.debug
 
